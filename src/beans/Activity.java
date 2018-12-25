a5796fc07429823558eda714254af36321c6bb6a
loki283@free.fr <loki283@free.fr>
2018-12-25 18:45:18 +0100

    Beans Activity and Person

--- src/beans/Activity.java
+++ src/beans/Activity.java
@@ -1,5 +1,109 @@
 package beans;
 
-public class Activity {
+import java.io.Serializable;
 
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Activity")
+public class Activity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id()
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	/*
+	 * Validation : birthday < year < now
+	 * UI : Primefaces.InputNumber
+	 * Mandatory : Yes
+	 */
+	@Basic(optional = false)
+	private int year;
+	
+	/*
+	 * Validation : None
+	 * UI : Primefaces.SelectOneMenu
+	 * Mandatory : Yes
+	 */
+	@Basic(optional = false)
+	private String type;
+	
+	/*
+	 * Validation : None
+	 * UI : p:inputText
+	 * Mandatory : Yes
+	 */
+	@Basic(optional = false)
+	private String title;
+	
+	/*
+	 * Validation : None
+	 * UI : Primefaces.InputTextarea (Autoresize)
+	 * Mandatory : No
+	 */
+	@Basic
+	private String description;
+	
+	/*
+	 * Validation : Yes but don't know what to use, regex ?
+	 * UI : p:inputText
+	 * Mandatory : No
+	 */
+	@Basic
+	private String websiteUrl;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getWebsiteUrl() {
+		return websiteUrl;
+	}
+
+	public void setWebsiteUrl(String websiteUrl) {
+		this.websiteUrl = websiteUrl;
+	}
 }
--- src/beans/Person.java
+++ src/beans/Person.java
@@ -1,5 +1,130 @@
 package beans;
 
-public class Person {
+import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "Person")
+public class Person implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id()
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	/*
+	 * Validation : Only characters, at least 2 chars, start with a capital letter. Regex ? (A-Z){1}[a-z]+
+	 * UI : p:inputText
+	 * Mandatory : Yes
+	 */
+	@Basic(optional = false)
+	private String lastname;
+	
+	/*
+	 * Validation : Only characters, at least 2 chars, start with a capital letter. Regex ? [A-Z][a-z]+
+	 * UI : p:inputText
+	 * Mandatory : Yes
+	 */
+	@Basic(optional = false)
+	private String firstname;
+	
+	/*
+	 * Validation : regex ? ^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$
+	 * UI : p:inputText
+	 * Mandatory : Yes
+	 */
+	@Basic(optional = false)
+	private String email;
+	
+	/*
+	 * Validation : Yes but don't know what to use, regex ?
+	 * UI : p:inputText
+	 * Mandatory : No
+	 */
+	@Basic
+	private String websiteUrl;
+	
+	/*
+	 * Validation : None
+	 * UI : Primefaces.Calendar
+	 * Mandatory : Yes
+	 */
+	@Basic(optional = false)
+	@Temporal(TemporalType.DATE)
+	private Date birthday;
+	
+	/*
+	 * Validation : None ? or maybe add min chars
+	 * UI : Primefaces.Password (Feedback)
+	 * Mandatory : Yes
+	 * Other : This should be the hash of the password, not the password ?
+	 */
+	@Basic(optional = false)
+	private String password;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getWebsiteUrl() {
+		return websiteUrl;
+	}
+
+	public void setWebsiteUrl(String websiteUrl) {
+		this.websiteUrl = websiteUrl;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
