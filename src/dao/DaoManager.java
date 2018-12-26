f91843128093a9310465737b794e33744abac284
loki283@free.fr <loki283@free.fr>
2018-12-26 23:42:53 +0100

    Replace Activity/PersonManager by a generic manager

--- src/dao/DaoManager.java
+++ src/dao/DaoManager.java
@@ -1,6 +1,8 @@
 package dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
@@ -9,9 +11,10 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
 import beans.Activity;
+import beans.Person;
 
 @Stateless
-public class ActivityManager {
+public class DaoManager {
 
 	@PersistenceContext(unitName = "dbCV")
     private EntityManager em;
@@ -21,26 +24,32 @@ public class ActivityManager {
         System.out.println("init " + this + " with " + em);
     }
 	
-	public Activity addActivity(Activity a) {
-		em.persist(a);
-		return a;
+	public <T> T add(T entity) {
+	    em.persist(entity);
+	    return entity;
+	}
+
+	public <T> T update(T entity) {
+	    return em.merge(entity);
+	}
+
+	public <T> void remove(Class<T> clazz, Long pk) {
+	    T entity = em.find(clazz, pk);
+	    if (entity != null)
+	        em.remove(entity);
 	}
 	
-	public Activity getActivity(Long id) {
-		return em.find(Activity.class, id);
+	public <T> T find(Class<T> clazz, Long id) {
+		return em.find(clazz, id);
 	}
 	
-	public Activity updateActivity(Activity a) {
-		return em.merge(a);
-	}
-	
-	public void removeActivity(Activity a) {
-		em.remove(a);
-	}
-	
-	private List<Activity> findByPerson(Long idPerson) {
+	public List<Activity> findActivitiesByPerson(Long idPerson) {
 		TypedQuery<Activity> query = em.createQuery("Select * from Activity where person = :idPerson", Activity.class);
 		query.setParameter("idPerson", idPerson);
 		return query.getResultList();
+	}
+	
+	public List<Person> findByName(String name) {
+		return null;
 	}
 }
