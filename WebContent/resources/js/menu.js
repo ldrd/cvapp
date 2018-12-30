e9ef20300b07739c595ec9ca1c747fa960c69ac5
loki283@free.fr <loki283@free.fr>
2018-12-30 14:40:24 +0100

    Debug search interface

--- WebContent/resources/js/menu.js
+++ WebContent/resources/js/menu.js
@@ -1,12 +1,11 @@
-document.addEventListener("click", function(event) {
-	if (event.target.closest(".menu-search")) return;
-	if (event.target.closest(".search-bar")) return;
-	
-	document.querySelector(".menu-search").classList.add("hidden");
+document.addEventListener("DOMContentLoaded", function(event) {
+	document.getElementsByClassName("menu-search")[0].classList.add("hidden");
 });
 
-function switchSearchResultVisibility() {
-	var element = document.getElementsByClassName("menu-search")[0];
-	if (element.classList.contains("hidden"))
-		element.classList.remove("hidden");
+function showSearchResult() {
+	document.getElementsByClassName("menu-search")[0].classList.remove("hidden");
+}
+
+function hideSearchResult() {
+	document.getElementsByClassName("menu-search")[0].classList.add("hidden");
 }
