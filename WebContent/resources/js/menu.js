4566ad0bcfc17d78955750c2d5263197a33093e3
loki283@free.fr <loki283@free.fr>
2019-01-01 14:06:38 +0100

    Debug menu quand clique outside

--- WebContent/resources/js/menu.js
+++ WebContent/resources/js/menu.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	document.getElementsByClassName("menu-search")[0].style.visibility = "hidden";
+	document.addEventListener("click", function(event) {
+		if (event.target.closest(".menu-search")) return;
+		if (event.target.closest(".search-bar")) return;
+
+		document.getElementsByClassName("menu-search")[0].style.visibility = "hidden";
+	});
 });
 
 function showSearchResult() {
